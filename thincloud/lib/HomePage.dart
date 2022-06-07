a1ebc0377a68a79382e1b65db6582a2c6cba8f11
emre.akkas <kemreakkas@gmail.com>
2022-06-07 23:25:26 +0300

    first commit

---
+++ thincloud/lib/HomePage.dart
@@ -0,0 +1,19 @@
+// ignore_for_file: file_names, unnecessary_import, implementation_imports
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
